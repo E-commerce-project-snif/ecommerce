4af529d65feafc7c54bf71784f21856bf7ca59be
ShadyKahouech <shady.kahouech@gmail.com>
2024-07-31 18:36:13 +0100

    save

--- client/ecommerce project 2/src/App.jsx
+++ client/ecommerce project 2/src/App.jsx
@@ -2,18 +2,32 @@ import "./App.css";
 import NavBar from "./components/NavBar.jsx";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home.jsx";
+import SignUp from "./components/SingnUp.jsx";
 import Login from "./components/Login.jsx";
+import ProductDetails from "./components/ProductDetails.jsx";
+import RelatedItem from "./components/RelatedItem.jsx";
 
 function App() {
   return (
-    <div className="navbar">
-      <NavBar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+    <div>
+      <div
+        className="navbar"
+        style={{
+          position: "-webkit-sticky",
+          position: "sticky",
+          top: "0px",
+          width: "100%",
+        }}
+      >
+        <NavBar />
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/details" element={<ProductDetails />} />
+        <Route path="/related" element={<RelatedItem />} />
+      </Routes>
     </div>
   );
 }
--- client/ecommerce project 2/src/components/SingnUp.jsx
+++ client/ecommerce project 2/src/components/SingnUp.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import gmail from "../assets/G.png";
 
 function SignUp() {
   return (
@@ -12,7 +13,10 @@ function SignUp() {
       <div className="flex flex-col w-60 gap-5">
         <h3 className="font-medium text-2xl">Create an account</h3>
         <span className="font-small">Enter your detail below</span>
-        
+        <input
+          placeholder="Name"
+          className="w-70 h-10 border-b-2 border-grey outline-none "
+        />
         <input
           placeholder="Email or phone number"
           className="w-70 h-70 border-b-2 border-grey outline-none"
@@ -22,8 +26,23 @@ function SignUp() {
           className="w-70 h-70 border-b-2 border-grey outline-none"
         />
         <button className="bg-red-600 text-white text-base p-2">
-         Log in
+          Create account
         </button>
+        <div className="sign in flex ">
+          <button
+            style={{ width: "300px" }}
+            className="box-border border-2 border-gray-500 p-2 bg-white text-black hover:bg-gray-100 flex items-center space-x-4 "
+          >
+            <img style={{ width: "30px" }} src={gmail} alt="" />
+            Sign up with google
+          </button>
+        </div>
+        <span>
+          Already have account ?{" "}
+          <a href="login" style={{ textDecoration: "underline" }}>
+            log in
+          </a>
+        </span>
       </div>
     </div>
   );
