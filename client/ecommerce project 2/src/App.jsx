6135bf06a514e30a39059342ffbe5dd1cf128b3d
Mednajmeddine <mohamednajmeddineb@gmail.com>
2024-08-01 14:20:24 +0100

    component MyAccount

--- client/ecommerce project 2/src/App.jsx
+++ client/ecommerce project 2/src/App.jsx
@@ -1,15 +1,18 @@
 // import { Routes, Route, Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home.jsx"
-import Footer from "./components/Footer.jsx"
+
+
 import { useState } from "react";
  
+
+
 import './App.css'
 import AdminDash from './components/AdminDash.jsx';
 import Category from './components/Category.jsx'
-import HomeDashboard from './components/HomeDashboard.jsx';
+  
 import Products from './components/Products.jsx';
 import Users from './components/Users.jsx';
+import MyAccount from "./components/MyAccount.jsx";
 
 
 
@@ -22,18 +25,19 @@ function App() {
   
 
   return (
-    <>
-     <BrowserRouter>
+    <div>
+     <Router>  
       <Routes>
         <Route path="/" >
-          <Route path='/admin' element={<AdminDash />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="products" element={<Products />} />
-          <Route path="/category" element={<Category />} />
+          <Route path='/admin'     element={<AdminDash />} />
+          <Route path="/users"     element={<Users />} />
+          <Route path="/products"  element={<Products />} />
+          <Route path="/category"  element={<Category />} />
+          <Route path="/myAccount" element={<MyAccount />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-    </>
+    </Router>
+    </div>
   )
 }
 
